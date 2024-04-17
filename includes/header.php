708a1f9c88bcc822c01bf91526f75ab2fdedd711
ivan <ivanjose_04@hotmail.com>
2024-04-17 18:26:37 -0400

    show button admin, and logout

--- includes/header.php
+++ includes/header.php
@@ -1,3 +1,32 @@
+<?php
+$host = 'localhost';
+$dbname = 'petshop';
+$user = 'root';
+$pass = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $user, $pass, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]);
+} catch (PDOException $e) {
+    die("problem: " . $e->getMessage());
+}
+
+$loggedIn = isset($_SESSION['user_id']); 
+$isAdmin = false;
+if (isset($_SESSION['user_id'])) {
+    $userId = $_SESSION['user_id'];
+    $stmt = $pdo->prepare("SELECT role_id FROM user WHERE id = ?");
+    $stmt->execute([$userId]);
+    $roleId = $stmt->fetchColumn();
+    if ($roleId == 1) {
+        $isAdmin = true;
+    }
+}
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -58,12 +87,24 @@
 
     <header>
         <nav>
-            <ul>
+            <ul style="display: flex; align-items: center;">
                 <li><a href="index.php">Home</a></li>
                 <li><a href="view/auth/register.php">Register</a></li>
-                <li><a href="view/auth/login.php">Login</a></li>
-                <li><a href="view/auth/profile.php">Profile</a></li>
-                <li> <a href="view/order/view_cart.php">Cart</a></li>
+                <?php if ($loggedIn) : ?>
+                    <li><a href="view/auth/profile.php">Profile</a></li>
+                    <li><a href="view/order/view_cart.php">Cart</a></li>
+                    <?php if ($isAdmin) : ?>
+                        <li><a href="view/admin/dashboard.php">Dashboard</a></li>
+                    <?php endif; ?>
+                    <li>
+                        <form action="index.php" method="post">
+                            <input type="hidden" name="logout" value="logout">
+                            <button type="submit" style="background-color: red; font-weight: bold;">Log out</button>
+                        </form>
+                    </li>
+                <?php else : ?>
+                    <li><a href="view/auth/login.php">Login</a></li>
+                <?php endif; ?>
             </ul>
         </nav>
         <div id="logo">
